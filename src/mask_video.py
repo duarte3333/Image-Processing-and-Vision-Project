Video masking code now 720p resolution (video file size was too large); fix indentation error.

src/mask_video.py
@@ -37,17 +37,24 @@ def bumper_mask(input_video, video_name, fps=60, codec="MJPG"):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+    # Reduce frame size to 720p resolution
+    new_width = 1280
+    new_height = 720
+
     fourcc = cv2.VideoWriter_fourcc(*codec)
-    video = cv2.VideoWriter(video_name, fourcc, fps, (width, height))
+    video = cv2.VideoWriter(video_name, fourcc, fps, (new_width, new_height))
 
     # Calculate the center and axes of the ellipse
-    center = (width // 2, height // 2)
-    axes = (width // 2, height // 2)
+    center = (new_width // 2, new_height // 2)
+    axes = (new_width // 2, new_height // 2)
 
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
+
+        # Resize the frame to 720p resolution
+        frame = cv2.resize(frame, (new_width, new_height))
 
         # Create a blue mask with the same size as the frame
         mask = np.zeros_like(frame)
@@ -60,7 +67,7 @@ def bumper_mask(input_video, video_name, fps=60, codec="MJPG"):
         frame_with_mask = cv2.bitwise_and(frame, mask)
 
         video.write(frame_with_mask)
-        cv2.imshow('Video with Mask', frame_with_mask)
+        cv2.imshow('BACK video with bumper mask', frame_with_mask)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
@@ -95,7 +102,7 @@ def left_mask(video_path, video_name, fps=60, codec="MJPG"):
         frame_with_mask[:, :width, :] = mask[:, :width, :]
 
         video.write(frame_with_mask)
-        cv2.imshow('Video with Mask', frame_with_mask)
+        cv2.imshow('LEFT video with mask', frame_with_mask)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
@@ -130,13 +137,13 @@ def right_mask(video_path, video_name, fps=60, codec="MJPG"):
         frame_with_mask[:, :width, :] = mask[:, :width, :]
 
         video.write(frame_with_mask)
-        cv2.imshow('Video with Mask', frame_with_mask)
+        cv2.imshow('RIGHT video with mask', frame_with_mask)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
-        cap.release()
-        cv2.destroyAllWindows()
-        video.release()
+    cap.release()
+    cv2.destroyAllWindows()
+    video.release()
 
 video_paths = ["video\TESLA_IST_ORIGINAL\TESLA_IST_ORIGINAL\\2023-04-29_16-40-01-back.mp4",
                "video\TESLA_IST_ORIGINAL\TESLA_IST_ORIGINAL\\2023-04-29_16-40-01-front.mp4",
@@ -149,7 +156,9 @@ left_masked = left_mask(video_paths[2], "left_video_masked.avi")
 right_masked = right_mask(video_paths[3], "right_video_masked.avi")
 
 
+"""
 #  Example usage:
-# input_video = "video\TESLA_IST_ORIGINAL\TESLA_IST_ORIGINAL\\2023-04-29_16-40-01-left_repeater.mp4"
-# video_name = "example_video_with_mask.avi"
-# left_mask(input_video, video_name)
+input_video = "video\TESLA_IST_ORIGINAL\TESLA_IST_ORIGINAL\\2023-04-29_16-40-01-left_repeater.mp4"
+video_name = "example_video_with_mask.avi"
+left_mask(input_video, video_name)
+"""